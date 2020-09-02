6f4a311ac7a537afd577c09983e40f378c603daa
NickForma <nickforma60@gmail.com>
2020-09-01 21:48:12 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 class="title">The Random Quizzz</h1>
+      <button class="start">Start</button>
+    </div>
+  );
+}
+
+export default App;
